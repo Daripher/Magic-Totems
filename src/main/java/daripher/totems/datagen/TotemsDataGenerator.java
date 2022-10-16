994851e0bae840e1973678da5020aa048598fe1a
Daripher <saelver.serbek@gmail.com>
2022-10-16 20:34:10 +0500

    Removed  totems generation data

--- src/main/java/daripher/totems/datagen/TotemsDataGenerator.java
+++ src/main/java/daripher/totems/datagen/TotemsDataGenerator.java
@@ -1,29 +1,12 @@
 package daripher.totems.datagen;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.google.gson.JsonElement;
-import com.mojang.serialization.JsonOps;
-
 import daripher.totems.TotemsMod;
-import daripher.totems.init.TotemsBiomeModifiers;
-import daripher.totems.init.TotemsPlacedFeatures;
-import net.minecraft.core.Registry;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DataProvider;
-import net.minecraft.resources.RegistryOps;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.common.data.JsonCodecProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 
 @EventBusSubscriber(modid = TotemsMod.MOD_ID, bus = Bus.MOD)
 public class TotemsDataGenerator
@@ -39,15 +22,5 @@ public class TotemsDataGenerator
 		dataGenerator.addProvider(event.includeClient(), new TotemsSoundDefinitionsProvider(dataGenerator, existingFileHelper));
 		
 		dataGenerator.addProvider(event.includeServer(), new TotemsLootTableProvider(dataGenerator));
-		dataGenerator.addProvider(event.includeServer(), forDataPackRegistry(dataGenerator, existingFileHelper, Registry.PLACED_FEATURE_REGISTRY, TotemsPlacedFeatures.REGISTRY));
-		dataGenerator.addProvider(event.includeServer(), forDataPackRegistry(dataGenerator, existingFileHelper, ForgeRegistries.Keys.BIOME_MODIFIERS, TotemsBiomeModifiers.REGISTRY));
-	}
-	
-	private static <T> DataProvider forDataPackRegistry(DataGenerator dataGenerator, ExistingFileHelper existingFileHelper, ResourceKey<Registry<T>> registryKey, DeferredRegister<T> registry)
-	{
-		RegistryAccess registryAccess = RegistryAccess.builtinCopy();
-		RegistryOps<JsonElement> registryOps = RegistryOps.create(JsonOps.INSTANCE, registryAccess);
-		Map<ResourceLocation, T> idToObjectMap = registry.getEntries().stream().collect(HashMap::new, (map, object) -> map.put(object.getId(), object.get()), (map1, map2) -> map1.putAll(map2));
-		return JsonCodecProvider.forDatapackRegistry(dataGenerator, existingFileHelper, TotemsMod.MOD_ID, registryOps, registryKey, idToObjectMap);
 	}
 }
